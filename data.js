f175046e804f9c66bfbb5500f0c3927ae68edd9c
Wyatt Queirolo <wfq2@cornell.edu>
2016-11-12 20:37:49 -0500

    data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 
 $( document ).ready(function() {
 	$.ajax({
-    url: 'http://api.reimaginebanking.com/atms?key=09a8dae9addf098be3a3d4ef4bc968b7',
+    url: 'http://api.reimaginebanking.com/accounts/5827bd04360f81f10454a22f/purchases?key=928cd0c9627ba32b3be6893c4c6c1d61
+',
     success: function(results){
     	$("div#testing").append(results);
         //do something 
