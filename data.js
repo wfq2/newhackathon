c77a4bdd5f30943d2fa940a22f415ddeb4dd1180
gh334 <gh334@cornell.edu>
2016-11-12 22:16:49 -0500

    learned how to get data yay

--- data.js
+++ data.js
@@ -1,11 +1,32 @@
+var timearray = [];
+var amountarray = [];
+
 
 $( document ).ready(function() {
 	$.ajax({
-    url: 'http://api.reimaginebanking.com/accounts/5827bd04360f81f10454a22f/purchases?key=928cd0c9627ba32b3be6893c4c6c1d61
-',
+    url: 'http://api.reimaginebanking.com/accounts/5827bd04360f81f10454a22f/purchases?key=928cd0c9627ba32b3be6893c4c6c1d61',
     success: function(results){
     	$("div#testing").append(results);
+    	var js_array = results;
+    	for (var i =0;i<js_array.length;i++){
+    		timearray.push(js_array[i].purchase_date);
+    		amountarray.push(js_array[i].amount);
+    		$("#totaldeposited").html(amountarray);
+    		$("numerdeposits")
+    	}
         //do something 
     }
 });
+});
+
+function gettotalsaved(amounts){
+	var total = 0;
+	for (int i=0;i<amounts.length;i++){
+		total += amounts[i];
+	}
+	return total;
+}
+
+$( "#paymentmade" ).click(function() {
+  
 });
--- index.php
+++ index.php
@@ -6,18 +6,7 @@ session_start();
 
     <head>
 	<title>TRIM</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="data.js"</script>
 	<title>Cornell Alpine Skiing</title>
-
-	<link rel="shortcut icon" type="image/x-icon" href="myicon.ico">
-	<meta name="google-site-verification" content="jLqIv-bYHodHnaXXeR_94O8GVGkyZ6ijJxWitVI65Zw" />
-	<meta name="keywords" content="cornell,skiing,cornell skiing,cornell alpine,cornell racing,cornell alpine skiing,cornell alpine racing,cornell ski team,cornell alpine ski team">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <meta name="description" content="Welcome to the official site of the Cornell Alpine Ski Team">
-
-<meta property="og:description"        content="Welcome to the Official Site of the Cornell Ski Team" />
-<meta property="og:image"              content="http://cornellalpineskiing.com/images/final%20logo2.png"/>
 
 
         <link href="CSS/styles.css" type="text/css" rel="stylesheet">
@@ -25,8 +14,8 @@ session_start();
         
 
         <?PHP
-        include("navigation.php");
-        require_once("config.php");
+        //include("navigation.php");
+        //require_once("config.php");
         ?>
 
 
@@ -34,10 +23,12 @@ session_start();
     </head>
 
     <body>
-
+    <div id="totaldeposited"></div>
+    <div id="numberdeposits"></div>
         
-        <?php require_once('footer.php'); ?>
-        </div>
+    <?php require_once('footer.php'); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="data.js"></script>
 
     </body>
 
